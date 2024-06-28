06971797b194ec11b6bd21cdcc0f363d287575e1
ERIC_BASKOVICH <eric.baskovich@gmail.com>
2024-06-28 15:32:12 -0600

    working image renderings

--- bestapp/src/App.jsx
+++ bestapp/src/App.jsx
@@ -1,66 +1,51 @@
-import { useState , useEffect} from 'react'
-import NavbarComp from './components/Navbar'
-import CarouselComp from './components/Carousel'
-import CardComp from './components/Card'
+import { useState, useEffect } from 'react';
+import NavbarComp from './components/Navbar';
+import CarouselComp from './components/Carousel';
+import CardComp from './components/Card';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import axios from 'axios';
-import './App.css'
+import './App.css';
 
 function App() {
+  const [newsJSON, setnewsJSON] = useState([]);
 
-  const [newsJSON, setnewsJSON] = useState([])
+  const getJson = async () => {
+    try {
+      const response = await axios("https://api.spaceflightnewsapi.net/v4/articles/?limit=6");
+      setnewsJSON(response.data.results);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
 
-  const getJson = async() => {
-    const response = await axios("https://api.spaceflightnewsapi.net/v4/articles/?limit=6")
-    setnewsJSON(response.data.results)
-  }
+  useEffect(() => {
+    console.log('Fetching Data')
+    getJson();
+  }, []);
 
-  
-// 
-  //API CALL FUNCTION 
-
-  useEffect(()=>{
-    getJson() 
-    // const print = async () => {console.log(await newsJSON)}
-    // print()
-    // console.log(newsJSON)
-  },[])
-
-  useEffect(()=>{
-    console.log(newsJSON)
-   
-  }, [newsJSON])
-
-  const randomId = () =>{
-    return Math.floor(Math.random()*100) + 1
-  }
+  useEffect(() => {
+    console.log(newsJSON);
+  }, [newsJSON]);
 
   return (
     <>
-     <NavbarComp />
-     <CarouselComp imagesrc="https://t4.ftcdn.net/jpg/05/56/16/81/360_F_556168144_qEINHd6qUsQH4b6wDvgOS0WtuShVwJVm.jpg "/>
-
-    <Container>
-      <Row>
-        <Col>
-          <CardComp imagesrc="https://media.istockphoto.com/id/1344443930/photo/space-shuttle-rocket-launch-in-the-sky-and-clouds-to-outer-space-sky-and-clouds-spacecraft.jpg?s=612x612&w=0&k=20&c=lYoFwMF9Sc6q07skiz6WaVovoseHk6M1tDr5qeecRjI="/>
-        </Col>
-        <Col>
-          <CardComp imagesrc="https://media.istockphoto.com/id/1344443930/photo/space-shuttle-rocket-launch-in-the-sky-and-clouds-to-outer-space-sky-and-clouds-spacecraft.jpg?s=612x612&w=0&k=20&c=lYoFwMF9Sc6q07skiz6WaVovoseHk6M1tDr5qeecRjI="/>
-        </Col>
-        <Col>
-          <CardComp imagesrc="https://media.istockphoto.com/id/1344443930/photo/space-shuttle-rocket-launch-in-the-sky-and-clouds-to-outer-space-sky-and-clouds-spacecraft.jpg?s=612x612&w=0&k=20&c=lYoFwMF9Sc6q07skiz6WaVovoseHk6M1tDr5qeecRjI="/>
-        </Col>
-      </Row>
-    </Container>
-
-     </>
-  )
+      <NavbarComp />
+      {newsJSON.length > 0 && <CarouselComp newsJSON={newsJSON} />}
+      <Container>
+        <Row>
+          {newsJSON.slice(0, 3).map((newsItem, index) => (
+            <Col key={index}>
+              <CardComp imagesrc={newsItem.image_url} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </>
+  );
 }
 
-export default App
+export default App;
 
-
-
+//PASS JSON to each card to render names and info
--- bestapp/src/components/Carousel.jsx
+++ bestapp/src/components/Carousel.jsx
@@ -2,50 +2,42 @@ import Carousel from 'react-bootstrap/Carousel';
 import { useState } from 'react';
 
 
-function CarouselComp({imagesrc}) {
-  const[slide1, setSlide1] = useState('')
-  const[slide2, setSlide2] = useState('')
-  const[slide3, setSlide3] = useState('')
 
+function CarouselComp({ newsJSON }) {
   
-  //getAPIFunction (prop)
-
-
   return (
     <Carousel data-bs-theme="dark">
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src={imagesrc}
+          src={newsJSON[3].image_url}
           alt="First slide"
         />
         <Carousel.Caption>
-          <h5>First slide label</h5>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <h1>{newsJSON[3].title}</h1>
+          <p>{newsJSON[3].summary}</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src={imagesrc}
+          src={newsJSON[4].image_url}
           alt="Second slide"
         />
         <Carousel.Caption>
-          <h5>Second slide label</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <h1>{newsJSON[4].title}</h1>
+          <p>{newsJSON[4].summary}</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src={imagesrc}
+          src={newsJSON[5].image_url}
           alt="Third slide"
         />
         <Carousel.Caption>
-          <h5>Third slide label</h5>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
+        <h1>{newsJSON[5].title}</h1>
+          <p>{newsJSON[5].summary}</p>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
